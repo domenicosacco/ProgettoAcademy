1f200a8f55a2f17ef5578d830e163b269d06a7c2
Domenico Sacco <domenico.sacco@alunos.ipb.pt>
2019-02-12 21:25:46 +0100

    fix days main page (regroup matches for championship day)

--- progetto-accademy/src/app/days/days.component.ts
+++ progetto-accademy/src/app/days/days.component.ts
@@ -3,6 +3,7 @@ import { DailyMatch } from '../models/dailyMatch';
 import {DayService} from '../day.service';
 import { ActivatedRoute } from '@angular/router';
 import { Match } from '../models/match';
+import { max } from 'rxjs/operators';
 
 @Component({
   selector: 'app-days',
@@ -12,7 +13,7 @@ import { Match } from '../models/match';
 export class DaysComponent implements OnInit {
 
   days: DailyMatch[]=[];
-  sortedDays=[];
+  sortedDays: DailyMatch[] =[];
 
   matches= [];
 
@@ -36,12 +37,12 @@ export class DaysComponent implements OnInit {
           let matchToPut = new Match();
 
           matchToPut.id = data['matches'][match].id,
-          matchToPut.homeTeamName=data['matches'][match].homeTeamName,
-          matchToPut.awayTeamName=data['matches'][match].awayTeamName,
-          matchToPut.homeTeamID=data['matches'][match].homeTeamID,
-          matchToPut.awayTeamID=data['matches'][match].awayTeamID,
-          matchToPut.homeTeamScore=data['matches'][match].homeTeamScore,
-          matchToPut.awayTeamScore=data['matches'][match].awayTeamScore,
+          matchToPut.homeTeamName=data['matches'][match]['homeTeam']['name'],
+          matchToPut.awayTeamName=data['matches'][match]['awayTeam']['name'],
+          matchToPut.homeTeamID=data['matches'][match]['homeTeam']['id'],
+          matchToPut.awayTeamID=data['matches'][match]['awayTeam']['id'],
+          matchToPut.homeTeamScore=data['matches'][match]['score']['fullTime']['homeTeam'],
+          matchToPut.awayTeamScore=data['matches'][match]['score']['fullTime']['awayTeam'],
           matchToPut.utcDate=data['matches'][match].utcDate,
           matchToPut.status=data['matches'][match].status,
           matchToPut.stage=data['matches'][match].stage,
@@ -57,22 +58,34 @@ export class DaysComponent implements OnInit {
           this.days.push(day);
 
         }
+      
+      let daysNumber=0;
+      for(let i=0;i<this.days.length;i++) {
+        let dayMatches=this.days[i].matches;
+        for(let j=0;j<dayMatches.length;j++) {
+          if(dayMatches[j].matchDay>daysNumber) {daysNumber=dayMatches[j].matchDay};
+        }
+      }
 
-        var mergedObj = this.days.reduce((acc, obj) => {
-          if (acc[obj.dayOfMatch]) {
-             acc[obj.dayOfMatch].matches = acc[obj.dayOfMatch].matches.isArray ? 
-             acc[obj.dayOfMatch].matches.concat(obj.matches) : 
-             [acc[obj.dayOfMatch].matches].concat(obj.matches);
-      
-          } else {
-            acc[obj.dayOfMatch] = obj;
+      for(let i=1;i<=daysNumber;i++) {
+        let dayToPut=new DailyMatch();
+        let matchesToAdd: Match[]=[];
+         for(let day of this.days) {
+           if (day.dayOfMatch==i) {dayToPut.competitionName=day.competitionName;
+          dayToPut.dayOfMatch=day.dayOfMatch;
+          for(let match of day.matches) {
+            if(!matchesToAdd.includes(match)) {matchesToAdd.push(match);}
           }
-      
-          return acc;
-      }, {});
-      
-      for (let prop in mergedObj) {
-        this.sortedDays.push(mergedObj[prop]) 
+          dayToPut.matches=matchesToAdd;
+          }
+         }
+         this.sortedDays.push(dayToPut);
+      }
+
+      for (let d of this.sortedDays) {
+        for(let match of d.matches) {
+        console.log(match);
+        }
       }
 
 
